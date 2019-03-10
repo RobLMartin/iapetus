443f14786e984f6ce25840c96c85a00e40251de7
Rob Martin <rmartin3@alumni.unca.edu>
2019-03-10 15:56:10 -0400

    basic setup

--- src/client/App.js
+++ src/client/App.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import AppBar from '@material-ui/core/AppBar';
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
 
 const Container = styled.div`
-   height: 100vh;
-   padding: 2em;
+    height: 100vh;
+    display: grid;
+    grid-template-areas: 
+        'sidenav appbar' 
+        'sidenav canvas';
+    grid-template-columns: 50px 1fr;
+    grid-template-rows: 50px 1fr;
 `
 
-const StyledAppBar = styled(AppBar)`
-    padding-left: 1em;
+const SideNav = styled(AppBar)`
+    grid-area: sidenav;  
+    background-color: #8862f1; 
+    z-index: 10000;
+     
+`
+
+const Canvas = styled.div`
+    grid-area: canvas;    
+`
+
+const TopBar = styled(AppBar)`
+    grid-area: appbar;    
+    background-color: white;
 `
 
 class App extends Component {
 
     render() {
+
         return (
             <Container>
-                <StyledAppBar position="relative">
-                    <h1>Hello, World!!!</h1>
-                </StyledAppBar>
+                <SideNav color="secondary" position="relative" />
+                <TopBar color="inhert" position="relative" />
+                <Canvas />
             </Container>
         )
     }
--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import './styles/style.sass'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>
+    , document.getElementById('root'))
